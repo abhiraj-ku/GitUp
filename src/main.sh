4c7941c941fc5a67b10db3e3907401885cf8f8a9
Abhishek Kumar <abhirajabhi312@gmail.com>
2024-09-26 21:42:22 +0530

    added support for win,lin,macos

--- src/main.sh
+++ src/main.sh
@@ -1,26 +1,31 @@
 #! /bin/bash
 
-# echo "Hello & welcome to GitUp your coding pal"
+echo "Hello & welcome to GitUp your coding pal"
 
-# include all source files/scripts
-#source ./src/commands.sh
-#source ./src/badges.sh
-#source ./src/points.sh
-#source ./src/utils.sh
+# Include all source files/scripts
+source ./src/commands.sh
+source ./src/badges.sh
+source ./src/points.sh
+source ./src/utils.sh
 
 function play_sound() {
     local sound_file="../assets/sounds/applause.mp3"
 
-    mpg123 -q "$sound_file" < /dev/null > /dev/null 2>&1 &
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        afplay "$sound_file" &>/dev/null &
+    elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        mpg123  "$sound_file" < /dev/null > /dev/null 2>&1 &  # runs in quiet mode and detaches the terminal
+    elif [[ "$OSTYPE" == "msys" ]]; then
+        # Windows
+        powershell -Command "(New-Object System.Media.SoundPlayer '$sound_file').Play()"
+    else 
+        echo "Unsupported OS"
+    fi
 }
-
 
 function sim_git_init(){
     echo "🚀 initializing a git repo ...."
-
     play_sound
-
-    
 }
 
 function main() {
